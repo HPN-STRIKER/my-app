46f416f86430aa133f9c65719435671e812fe717
Fabian <fabianpatrick1970@gmail.com>
2022-05-29 23:32:53 +0200

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+    const myWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    myWindow.loadFile('index.html');
+});
