976dc7a811b6f38b3b55586b7cc69357af2d4ecc
Fabian <fabianpatrick1970@gmail.com>
2022-05-31 15:15:13 +0200

    text display

--- html-manager.js
+++ html-manager.js
@@ -1,3 +1,4 @@
+// Pops up the new note window
 let popup = document.getElementById("popup");
 function openPopup() {
     popup.classList.add("open-popup");
@@ -6,4 +7,13 @@ function closePopup() {
     popup.classList.remove("open-popup");
 }
 
+// Display "Click the + button to add a note"
+var emptyLeftBox = document.getElementById('leftBox').innerHTML === "";
+let leftBox = document.getElementById("leftBox")
 
+if(emptyLeftBox){
+    leftBox.classList.remove("not-empty-left-box");
+}
+else{
+    leftBox.classList.add("not-empty-left-box");
+}
